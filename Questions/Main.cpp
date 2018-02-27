bebbde052d4978fdfa83bbd97c29891338a1a257
shanu-pandey <shantanu.067@gmail.com>
2018-02-26 20:50:13 -0700

    continue...

--- Questions/Main.cpp
+++ Questions/Main.cpp
@@ -3,6 +3,13 @@
 #include <vector>
 using namespace std;
 
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
 class TreeNode {
 public:
 	int data;
@@ -293,6 +300,15 @@ int TreeHeight(TreeNode* root)
 		return rightHeight + 1;
 }
 
+vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+	vector<Interval> result;
+	for (int i = 0; i < intervals.size(); i++)
+	{
+		
+	}
+	return result;
+}
+
 int main()
 	{
 #pragma region Question1: Longest substring without repeating characters
@@ -445,6 +461,21 @@ int main()
 
 		int h = TreeHeight(n);
 
+#pragma endregion
+
+#pragma region Question9: Insert Interval
+		/*
+		Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+		You may assume that the intervals were initially sorted according to their start times.
+
+		Example 1:
+		Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+		Example 2:
+		Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+		This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+		*/
 #pragma endregion
 
 		return 0;
