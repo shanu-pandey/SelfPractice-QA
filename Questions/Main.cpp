0b0430182aa67e34cb95f393178e0a1b9a6166f9
Shantanu <shantanu.067@gmail.com>
2019-07-09 19:10:04 -0600

    string IsUnique complete

--- Questions/Main.cpp
+++ Questions/Main.cpp
@@ -10,17 +10,60 @@
 
 using namespace std;
 
-bool HasUnique(string i_str)
+bool HasUnique_ArrayDS(string i_str)
 {
-	bool bResult = false;
+	bool temp[26] = { false };
+	int length = i_str.length();
+	if (length < 2)
+		return true;
+	for (int i = 0; i < length; i++)
+	{
+		int index = i_str[i] - 'a';
+		if (temp[index] == true)
+			return false;
+		else
+			temp[index] = true;
+	}
+	return true;
+}
+bool HasUnique_NoDS(string i_str)
+{
+	int length = i_str.length();
+	if (length < 2)
+		return true;
 
-	return bResult;
+	int checker = 0;
+	for (int i = 0; i < length; i++)
+	{
+		int val = i_str[i] - 'a';
+
+		if ((checker &(1 << val)) > 0)
+			return false;
+
+		checker |= (1 << val);
+	}
+	return true;
 }
 
 int main()
 	{
 #pragma region String: IsUnique
+	/*Implement an algorithm to determine if a string has all unique characters. 
+	What if you cannot use additional data structures?*/
+	string input = "qawsedrftgy";
+	bool res = HasUnique_ArrayDS(input);
+	res = HasUnique_NoDS(input);
 #pragma endregion
+
+#pragma region String: IsUnique
+	/*Implement an algorithm to determine if a string has all unique characters.
+	What if you cannot use additional data structures?*/
+	string input = "qawsedrftgy";
+	bool res = HasUnique_ArrayDS(input);
+	res = HasUnique_NoDS(input);
+#pragma endregion
+
+
 		return 0;
 	}
 
