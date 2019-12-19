3846aa96797302d7a4b4e4a651a39452ea125215
Shantanu <shantanu.067@gmail.com>
2019-12-18 23:57:42 -0700

    more questions

--- Questions/Main.cpp
+++ Questions/Main.cpp
@@ -73,7 +73,7 @@ std::vector<std::vector<int>> PairEqualSum(int i_array[], int i_size)
 int Factorial(int n)
 {
 	if (n > 1)
-		return n * factorial(n - 1);
+		return n * Factorial(n - 1);
 	else
 		return 1;
 }
@@ -131,6 +131,81 @@ Node* reverseBetween(Node* head, int m, int n)
 }
 
 
+vector<int> GetConnections(int src, vector<int> &T)
+{
+	vector<int> paths;
+
+	for (int i = 1; i < T.size(); i++)
+	{
+		if (T[i] == src)
+			paths.push_back(i);
+	}
+	return paths;
+}
+
+
+void GetCost(int curr, vector<int> roads, int cost, bool bTicketUsed, vector<int>& T, int& maxCost)
+{
+	if (cost > maxCost)
+		maxCost = cost;
+	for (auto it : roads)
+	{
+		if (it % 2 != 0)
+		{
+			if (bTicketUsed)
+			{
+				bTicketUsed = false;
+				return;
+			}
+			else
+				bTicketUsed = true;
+		}
+		GetCost(it, GetConnections(it, T), cost + 1, bTicketUsed, T, maxCost);
+	}
+}
+
+
+int solution(vector<int> &T)
+{	
+	int maxCost = 1;
+	GetCost(0, GetConnections(0, T), 1, false, T, maxCost);
+	return maxCost;
+}
+
+
+void Reverse(char* start, char* end)
+{
+	char temp;
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*start++ = *end;
+		*end-- = temp;
+	}
+}
+
+void ReverseWordsInPlace(char* input)
+{
+	char* begin = input;
+	char* temp = input;
+
+	while (*temp)
+	{
+		temp++;
+		if (*temp == '/0')
+		{
+			Reverse(begin, temp);
+		}
+		else if (*temp == ' ')
+		{
+			Reverse(begin, temp - 1);
+			begin = temp + 1;
+		}
+	}
+	Reverse(input, temp - 1);
+}
+
 int main()
 	{
 #pragma region How do you find the missing number in a given integer array of 1 to 100 ?
@@ -145,7 +220,7 @@ int main()
 
 #pragma region How do you find all pairs of an integer array whose sum is equal to a given number?
 	int a[] = { 1,1,2,3,5,5,6,7,8,9,10,10,11,11,12,12,12,12 };
-	std::vector<int> res = DuplicateNumbers(a,18);
+	//std::vector<int> res = DuplicateNumbers(a,18);
 #pragma endregion
 
 #pragma region Reverse a linked list from position m to n in one - pass.
@@ -161,8 +236,11 @@ int main()
 	n3->next = n4;
 	n4->next = n5;
 	
-	Node* rev = reverseBetween(n1, 2, 4);
+	//Node* rev = reverseBetween(n1, 2, 4);
 #pragma endregion
-		return 0;
+		
+	char* input = "This is a test input to test things!";
+	ReverseWordsInPlace(input);
+	   	return 0;
 	}
 
