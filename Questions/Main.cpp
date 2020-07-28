ac2d25e5a57ec35ebfd7cb321df01cef35eeed8c
Shantanu <shantanu.067@gmail.com>
2020-07-27 23:49:09 -0600

    some mopre practice

--- Questions/Main.cpp
+++ Questions/Main.cpp
@@ -2179,6 +2179,40 @@ int bstDistance(int num, vector<int> values, int node1, int node2)
 	return distance;
 }
 
+void CountHelper(vector<vector<char>>& grid, int i, int j)
+{
+	if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1')
+		return;
+
+	grid[i][j] = '2';
+
+	CountHelper(grid, i - 1, j);
+	CountHelper(grid, i + 1, j);
+	CountHelper(grid, i, j - 1);
+	CountHelper(grid, i, j + 1);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+	if (grid.size() == 0)
+		return 0;
+	int row = grid.size();
+	int col = grid[0].size();
+	int result = 0;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (grid[i][j] == '1')
+			{
+				CountHelper(grid, i, j);
+				result++;
+			}
+		}
+	}
+
+	return result;
+}
+
 int main()
 	{
 	vector<string> res = subStringsLessKDist("democracy", 5);
@@ -2763,9 +2797,38 @@ Output:
 
 		Node_R* rooted = newNode(0);
 		CreateTree(rooted, test);
-		int d = Solution_Path_R(test);// (root, true);
+		int d = Solution_Path_R(test);// (root, true);		
 
+#pragma region Number of Islands
 
+		/*Given a 2d grid map of '1's(land) and '0's(water), count the number of islands.
+		An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+		You may assume all four edges of the grid are all surrounded by water.
+
+		Input: grid = [
+		  ["1","1","1","1","0"],
+		  ["1","1","0","1","0"],
+		  ["1","1","0","0","0"],
+		  ["0","0","0","0","0"]
+		]
+		Output: 1
+
+		Input: grid = [
+		  ["1","1","0","0","0"],
+		  ["1","1","0","0","0"],
+		  ["0","0","1","0","0"],
+		  ["0","0","0","1","1"]
+		]
+		Output: 3
+
+		vector<vector<char>> island = { {'1','1','1','1','0'},
+		{'1','1','0','1','0'},
+		{'1','1','0','0','0'},
+		{'0','0','0','0','0'}
+		};
+		int cccc = numIslands(island);
+		*/
+#pragma endregion
 		
 		return 0;
 	}
