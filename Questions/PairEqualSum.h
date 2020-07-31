26600578fb558f4d6d88d314855779e92a9645eb
Shantanu <shantanu.067@gmail.com>
2020-07-31 00:20:52 -0600

    pair equal sum done

--- Questions/PairEqualSum.h
+++ Questions/PairEqualSum.h
@@ -8,6 +8,22 @@ namespace Array
 	class PairEqualSum
 	{
 	public:
-		std::vector<std::pair<int, int>> GetAllSumPair(int[] i_numbers, int i_sum);
+		static std::vector<std::pair<int, int>> GetAllSumPair(int i_numbers[], int i_sum);
+	};
+}
+
+std::vector<std::pair<int, int>> Array::PairEqualSum::GetAllSumPair(int i_numbers[], int i_sum)
+{
+	std::vector<std::pair<int, int>> result;
+	int sz = sizeof(i_numbers) / sizeof(int);
+
+	for (int i = 0; i < sz-1; i++)
+	{
+		for (int j = i + 1; j < sz; j++)
+		{
+			if (i_numbers[i] + i_numbers[j] == i_sum)
+				result.push_back(std::make_pair(i_numbers[i], i_numbers[j]));
+		}
 	}
+	return result;
 }
