27079b6abc5973917586a6eab2f021f535e45b7b
Shantanu <shantanu.067@gmail.com>
2020-08-06 23:39:40 -0600

    valid parentheses complete

--- Questions/ValidParentheses.h
+++ Questions/ValidParentheses.h
@@ -28,6 +28,7 @@ Output: true
 */
 #pragma once
 #include <string>
+#include <stack>
 
 namespace Stack
 {
@@ -39,9 +40,40 @@ namespace Stack
 }
 
 
-bool Stack::ValidParentheses::IsValid(std::string i_str)
+bool CheckParentheses(char i_left, char i_right)
 {
-	bool result = false;
+	if (i_left == '(' && i_right == ')')
+		return true;
+	if (i_left == '[' && i_right == ']')
+		return true;
+	if (i_left == '{' && i_right == '}')
+		return true;
 
-	return result;
+	return false;
+}
+
+bool Stack::ValidParentheses::IsValid(std::string i_str)
+{	
+	if (i_str.length() < 2)
+		return false;
+
+	std::stack<char> visited;
+
+	for (int i = 0; i < i_str.length(); i++)
+	{
+		if (visited.empty())
+			visited.push(i_str[i]);
+		else
+		{
+			if (i_str[i] == '[' || i_str[i] == '{' || i_str[i] == '(')
+				visited.push(i_str[i]);
+			else if (!CheckParentheses(visited.top(), i_str[i]))
+				return false;
+			else
+				visited.pop();
+		}
+	}
+	if (visited.empty())
+		return true;
+	return false;
 }
